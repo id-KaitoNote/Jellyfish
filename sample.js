02a9a64a9c16380f6c613ff2ee90bd718885457c
Note-Kaito <50510177+Note-Kaito@users.noreply.github.com>
2019-09-17 00:15:06 +0900

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,59 @@
+//
+// //IDを取得
+// var target = document.getElementById('dropdown');
+//
+// //「target」にマウスが乗った時にイベントを実行する
+// target.onmouseenter = function() {
+//   console.log('OK!');
+// };
+
+
+$(function(){
+    $('.dropdown-child').mouseenter(function(){
+      //親リストにマウスが重なった時に重なった親リストの子リストだけを表示させる
+        $(this).children("ul:not(animated)").slideDown("slow");
+    }),
+    $('.dropdown-child').mouseleave(function(){
+      //リストからマウスが外れたらスライドしながらリストを消す
+        $(this).children("ul:not(animated)").slideUp("fast");
+    });
+});
+
+// $(function(){
+//     $('.dropdown-child').hover(function(){
+//       //親リストにマウスが重なった時に重なった親リストの子リストだけを表示させる
+//         $(this).children("ul").slideToggle();
+//     });
+// });
+// 『Toggle』 ver
+
+$(function(){
+$(".title1").delay(4500)
+            .css({
+              left:"-100px",
+              opacity:"0.0"
+          }).animate({
+              left:"100px",
+              opacity:"1.0"},2000
+            )
+          });
+$(function(){
+$(".title2").delay(5000)
+            .css({
+              left:"-100px",
+              opacity:"0.0"
+          }).animate({
+              left:"100px",
+              opacity:"1.0"},2000
+            )
+          });
+$(function(){
+$(".title3").delay(5500)
+            .css({
+              left:"-100px",
+              opacity:"0.0"
+          }).animate({
+              left:"100px",
+              opacity:"1.0"},2000
+            )
+          });
