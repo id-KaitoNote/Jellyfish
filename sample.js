84ef3b81cbb6ac61dfc3de34c4e4bc6a6321c36b
Kaito Note <fcb101010soccer@ezweb.ne.jp>
2019-09-18 20:22:21 +0900

    test

--- sample.js
+++ sample.js
@@ -1,34 +1,64 @@
 //
 // //IDを取得
-// var target = document.getElementById('dropdown');
+// var i = document.getElementById('dropdown');
+// var c = document.querySelectorAll('.dropdown-child');
 //
 // //「target」にマウスが乗った時にイベントを実行する
-// target.onmouseenter = function() {
-//   console.log('OK!');
+// c.mouseenter = function() {
+//   console.log("OK!!!!!!!!!!!!!!!!!!!!!!!!!");
 // };
+//
 
+//
+// var c = document.querySelector('.dropdown-child');
+// var m = document.querySelector('.dropdown_menu');
+//
+//   c.addEventListener('mouseenter', function() {
+//     console.log('OK!!!!!!!!!!!!!!');
+//     m.style.display = 'block';
+//   });
+//
+//   c.addEventListener('mouseleave', function() {
+//     console.log('NG!!!!!!!!!!!!!!!!!');
+//     m.style.display = 'none';
+//   });
+
+// var c = document.querySelector('.dropdown-child');
+// var m = document.querySelector('.dropdown_menu');
+//
+// // for (let i = 0; i < 2; i++) {
+//
+//   c.addEventListener('mouseenter', () => {
+//     console.log('OK!!!!!!!!!!!!!!');
+//     m.style.display = 'block';
+//   });
+//
+//   c.addEventListener('mouseleave', () => {
+//     console.log('NG!!!!!!!!!!!!!!!!!');
+//     m.style.display = 'none';
+//   });
+// }
 
 $(function(){
     $('.dropdown-child').mouseenter(function(){
       //親リストにマウスが重なった時に重なった親リストの子リストだけを表示させる
-        $(this).children("ul:not(animated)").slideDown("slow");
+        $(this).children("ul").slideDown("slow");
     }),
     $('.dropdown-child').mouseleave(function(){
       //リストからマウスが外れたらスライドしながらリストを消す
-        $(this).children("ul:not(animated)").slideUp("fast");
+        $(this).children("ul").slideUp("fast");
     });
 });
 
-// $(function(){
-//     $('.dropdown-child').hover(function(){
-//       //親リストにマウスが重なった時に重なった親リストの子リストだけを表示させる
-//         $(this).children("ul").slideToggle();
-//     });
-// });
-// 『Toggle』 ver
+$(function(){
+    $('.dropdown-child').hover(function(){
+      //親リストにマウスが重なった時に重なった親リストの子リストだけを表示させる
+        $(this).children("ul").slideToggle();
+    });
+});
+『Toggle』 ver
 
 if (window.matchMedia( "(max-width: 768px)" ).matches) {
-
 
   $(function(){
   $(".title1").delay(4500)
